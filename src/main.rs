4ba1907f562ed15b740aaaf2bf0d3eb5ffd1d431
Ruud van Asseldonk <dev@veniogames.com>
2022-10-16 22:58:21 +0200

    Add a cache for storing compressed objects
    Or well, the objects are stored in the Git database, but we store the
    mapping of unminified blob to the various minified and compressed blobs
    in a tsv file, so we can locate those blobs later.
    
    Currently the blobs could be garbage-collected, but the plan is to build
    a Git tree, and then a commit, and then a ref, which would ensure that
    the blobs are not dangling.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,90 @@
+use std::collections::BTreeMap;
+use std::fs;
+use std::io;
+
 use git2::{BranchType, ObjectType, Oid, Repository, Tree};
 
 type Result<T> = std::result::Result<T, git2::Error>;
+
+/// Blob oids of an html blob that we have already minified in the past.
+#[derive(Debug)]
+struct MinifiedBlobs {
+    minified: Oid,
+    gz: Oid,
+    br: Oid,
+}
+
+/// A cache of minified and comprssed blobs.
+///
+/// We use a B-tree map here instead of a hash map to ensure that we can
+/// serialize in sorted order, to keep the output deterministic. The overhead
+/// of the lookup is small anyway compared to compression.
+struct Cache(BTreeMap<Oid, MinifiedBlobs>);
+
+impl Cache {
+    const HEADER: &'static str = "blob\tminified\tgz\tbr";
+
+    pub fn new() -> Self {
+        Self(BTreeMap::new())
+    }
+
+    fn serialize<W: io::Write>(&self, mut out: W) -> std::io::Result<()> {
+        writeln!(out, "{}", Self::HEADER)?;
+        for (k, v) in self.0.iter() {
+            writeln!(
+                out,
+                "{}\t{}\t{}\t{}",
+                k.to_string(),
+                v.minified.to_string(),
+                v.gz.to_string(),
+                v.br.to_string(),
+            )?;
+        }
+        Ok(())
+    }
+
+    fn deserialize<R: io::BufRead>(input: R) -> std::io::Result<Self> {
+        let mut result = BTreeMap::new();
+        let mut lines = input.lines();
+
+        match lines.next() {
+            None => panic!("Failed to load cache, expected header row."),
+            Some(row) => assert_eq!(row?, Self::HEADER, "Invalid header row."),
+        }
+
+        // Skip the header row, it is just for clarity.
+        for line_opt in lines {
+            let line = line_opt?;
+            let mut parts = line.split('\t');
+            let mut next_oid = || {
+                Oid::from_str(parts.next().expect("Invalid format, expected oid."))
+                    .expect("Invalid oid.")
+            };
+            result.insert(
+                next_oid(),
+                MinifiedBlobs {
+                    minified: next_oid(),
+                    gz: next_oid(),
+                    br: next_oid(),
+                },
+            );
+        }
+
+        Ok(Cache(result))
+    }
+
+    pub fn save(&self, fname: &str) -> io::Result<()> {
+        let f = fs::File::create(fname)?;
+        let writer = io::BufWriter::new(f);
+        self.serialize(writer)
+    }
+
+    pub fn load(fname: &str) -> io::Result<Self> {
+        let f = fs::File::open(fname)?;
+        let reader = io::BufReader::new(f);
+        Self::deserialize(reader)
+    }
+}
 
 fn compress_zopfli(input: &[u8]) -> Vec<u8> {
     let opts = zopfli::Options {
@@ -29,7 +113,6 @@ fn compress_brotli(input: &[u8]) -> Vec<u8> {
         mode: BrotliEncoderMode::BROTLI_MODE_TEXT,
         ..BrotliEncoderParams::default()
     };
-    println!("Brotli: {:?}", params);
 
     brotli::enc::BrotliCompress(&mut input, &mut output, &params)
         .expect("Brotli compression should not fail, we don't do IO here.");
@@ -37,7 +120,7 @@ fn compress_brotli(input: &[u8]) -> Vec<u8> {
     output
 }
 
-fn minimize_blob(repo: &Repository, _name: &str, id: Oid) -> Result<Oid> {
+fn minimize_blob(repo: &Repository, id: Oid) -> Result<MinifiedBlobs> {
     let blob = repo.find_blob(id)?;
 
     // TODO: Cache this.
@@ -58,9 +141,6 @@ fn minimize_blob(repo: &Repository, _name: &str, id: Oid) -> Result<Oid> {
     let gz_bytes = compress_zopfli(&minified_bytes[..]);
     let br_bytes = compress_brotli(&minified_bytes[..]);
 
-    // Store the minified version in a blob.
-    let blob_raw = repo.blob(&minified_bytes[..])?;
-
     println!(
         "  -> shrunk {} to {} ({:.1}%), gzipped to {} ({:.1}%), brotlid to {} ({:.1}%)",
         blob.size(),
@@ -72,12 +152,32 @@ fn minimize_blob(repo: &Repository, _name: &str, id: Oid) -> Result<Oid> {
         100.0 * br_bytes.len() as f32 / blob.size() as f32,
     );
 
-    // TODO: Actually, instead of returning the oid, we should probably append
-    // an entry to some in-progress tree.
-    Ok(blob_raw)
+    // Store the minified version in a blob.
+    let result = MinifiedBlobs {
+        minified: repo.blob(&minified_bytes[..])?,
+        gz: repo.blob(&gz_bytes[..])?,
+        br: repo.blob(&br_bytes[..])?,
+    };
+
+    Ok(result)
 }
 
-fn minimize_tree(repo: &Repository, tree: &Tree) -> Result<()> {
+fn minimize_blob_cached<'a>(
+    cache: &'a mut Cache,
+    repo: &Repository,
+    id: Oid,
+) -> Result<&'a MinifiedBlobs> {
+    use std::collections::btree_map::Entry;
+
+    let blobs = match cache.0.entry(id) {
+        Entry::Occupied(o) => o.into_mut(),
+        Entry::Vacant(v) => v.insert(minimize_blob(repo, id)?),
+    };
+
+    Ok(blobs)
+}
+
+fn minimize_tree(cache: &mut Cache, repo: &Repository, tree: &Tree) -> Result<()> {
     for entry in tree.iter() {
         println!(
             "{:?} {}",
@@ -88,13 +188,13 @@ fn minimize_tree(repo: &Repository, tree: &Tree) -> Result<()> {
         match entry.kind() {
             Some(ObjectType::Tree) => {
                 let subtree = repo.find_tree(entry.id())?;
-                minimize_tree(repo, &subtree)?;
+                minimize_tree(cache, repo, &subtree)?;
             }
             Some(ObjectType::Blob) => {
                 let name = entry.name().expect("Huh, unnamed tree entry?");
                 if name.ends_with(".html") {
-                    let new_oid = minimize_blob(repo, name, entry.id())?;
-                    println!(" -> {:?}", new_oid);
+                    let blobs = minimize_blob_cached(cache, repo, entry.id())?;
+                    println!(" -> {:?}", blobs);
                 }
             }
             ot => panic!("Unexpected object type in tree: {:?}", ot),
@@ -104,12 +204,12 @@ fn minimize_tree(repo: &Repository, tree: &Tree) -> Result<()> {
     Ok(())
 }
 
-fn minimize(repo: &mut Repository) -> Result<()> {
+fn minimize(cache: &mut Cache, repo: &Repository) -> Result<()> {
     let pages_branch = repo.find_branch("gh-pages", BranchType::Local)?;
     println!("Branch: {:?}", pages_branch.get().target());
     let tree = pages_branch.get().peel_to_tree()?;
 
-    minimize_tree(&repo, &tree)?;
+    minimize_tree(cache, repo, &tree)?;
 
     Ok(())
 }
@@ -120,9 +220,20 @@ fn main() -> Result<()> {
     args.next();
 
     let repo_path = args.next().expect("Expected repository path.");
+    let repo = Repository::open(repo_path)?;
 
-    let mut repo = Repository::open(repo_path)?;
-    minimize(&mut repo)?;
+    let mut cache = match Cache::load("cache.tsv") {
+        Ok(cache) => cache,
+        Err(err) => {
+            println!("Starting with empty cache, cache failed to load: {:?}", err);
+            Cache::new()
+        }
+    };
+
+    minimize(&mut cache, &repo)?;
+
+    cache.save("cache.tsv.new").expect("Failed to save cache.");
+    std::fs::rename("cache.tsv.new", "cache.tsv").expect("Failed to move cache.");
 
     Ok(())
 }

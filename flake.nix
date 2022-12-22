2fca2b0f824e315a55afc618ce9e915c66a00149
Ruud van Asseldonk <dev@veniogames.com>
2022-12-22 21:38:20 +0100

    Build a static binary with Nix
    After fiddling with this for SO LONG, FINALLY I got it to work! Hooray!
    
    The trick was to use pkgsStatic everywhere, but that was not enough, we
    needed to pass a few -l flags to rustc to make it link the right
    libraries. Oh and we need to override libgit2 to not depend on
    http-parser.

--- flake.nix
+++ flake.nix
@@ -5,47 +5,48 @@
 
   outputs = { self, nixpkgs }: 
     let
-      supportedSystems = [ "x86_64-linux" ];
-      # Ridiculous boilerplate required to make flakes somewhat usable.
-      forEachSystem = f:
-        nixpkgs.lib.zipAttrsWith
-          (name: values: builtins.foldl' (x: y: x // y) {} values)
-          (map
-            (k: builtins.mapAttrs (name: value: { "${k}" = value; }) (f k))
-            supportedSystems
-          );
+      name = "minimizer";
+      version = "0.1.0";
+      pkgs = import nixpkgs { system = "x86_64-linux" ; };
+
+      # The default libgit2 package is incompatible with static builds, because
+      # its dependency http-parser is built as a dynamic library. But we don't
+      # need it anyway, it is vendored by the libgit2 crate.
+      libgit2Fixed = (pkgs.pkgsStatic.libgit2.override {
+        staticBuild = true;
+        http-parser = null;
+      }).overrideAttrs (oldAttrs: {
+        cmakeFlags = pkgs.lib.remove "-DUSE_HTTP_PARSER=system" oldAttrs.cmakeFlags;
+      });
     in
-      forEachSystem (system:
-        let
-          name = "minimizer";
-          version = "0.1.0";
-          pkgs = import nixpkgs { inherit system; };
-        in
-          rec {
-            packages = {
-              default = pkgs.rustPlatform.buildRustPackage rec {
-                inherit name version;
-                src = pkgs.lib.sourceFilesBySuffices ./. [
-                  ".rs"
-                  "Cargo.lock"
-                  "Cargo.toml"
-                ];
-                cargoLock = {
-                  lockFile = ./Cargo.lock;
-                  outputHashes = {
-                    "brotli-sys-0.3.2" = "sha256-knjFVyjiW03DA5wLw7VxQmaqUJXE6B8Zs0CvoO16QaI=";
-                  };
-                };
-                nativeBuildInputs = [
-                  pkgs.pkg-config
-                ];
-                buildInputs = [
-                  pkgs.brotli
-                  pkgs.libgit2
-                  pkgs.openssl
-                ];
-              };
+      {
+        packages.x86_64-linux.ls = pkgs.pkgsStatic.pcre;
+
+        packages.x86_64-linux.default = pkgs.pkgsStatic.rustPlatform.buildRustPackage rec {
+          inherit name version;
+          src = pkgs.lib.sourceFilesBySuffices ./. [
+            ".rs"
+            "Cargo.lock"
+            "Cargo.toml"
+          ];
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "brotli-sys-0.3.2" = "sha256-knjFVyjiW03DA5wLw7VxQmaqUJXE6B8Zs0CvoO16QaI=";
             };
-          }
-      );
+          };
+          nativeBuildInputs = [
+            pkgs.pkgsStatic.pkg-config
+          ];
+          buildInputs = with pkgs.pkgsStatic; [
+            brotli
+            openssl
+            libssh2
+            pcre
+            zlib
+            libgit2Fixed
+          ];
+          RUSTFLAGS = "-lbrotlidec -lbrotlicommon -lbrotlienc -lm -lssl -lc";
+        };
+      };
 }
